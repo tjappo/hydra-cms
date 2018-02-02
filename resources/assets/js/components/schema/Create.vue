bdd3682f92e1707e43ba98272b1df51a80979eb7
Tjappo <yj.hu@ziggo.nl>
2018-02-02 23:09:44 +0100

    added first version of create schema

--- resources/assets/js/components/schema/Create.vue
+++ resources/assets/js/components/schema/Create.vue
@@ -8,18 +8,64 @@
             <div class="col">
                 <div class="card w-100">
                     <div class="card-header">
-                        Create new schema: <strong>{{test.title}}</strong>
+                        Create new schema: <strong>{{data.title}}</strong>
                     </div>
                     <div class="card-body">
                         <div class="form-group">
                             <label for="title">Title</label>
-                            <input id="title" type="text" class="form-control" v-model="test.title">
+                            <input id="title" type="text" class="form-control" v-model="data.title">
                         </div>
-
+                        <table class="table" v-if="itemsExists">
+                            <thead>
+                            <tr>
+                                <td><strong>Name</strong></td>
+                                <td><strong>Type</strong></td>
+                                <td><strong>Description</strong></td>
+                                <td><strong>Default value</strong></td>
+                                <td><strong>Hidden</strong></td>
+                                <td></td>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr v-for="(item, index) in data.items">
+                                <td>
+                                    <input type="text" class="form-control" placeholder="Name" v-model="item.name"
+                                           required>
+                                </td>
+                                <td>
+                                    <select class="form-control" required v-model="item.type">
+                                        <option value="string">String</option>
+                                        <option value="integer">Number</option>
+                                        <option value="boolean">Boolean</option>
+                                        <option value="upload">Media upload</option>
+                                        <option value="html">HTML</option>
+                                    </select>
+                                </td>
+                                <td>
+                                    <input type="text" class="form-control" placeholder="Description"
+                                           v-model="item.description">
+                                </td>
+                                <td>
+                                    <input type="text" class="form-control" placeholder="Default value">
+                                </td>
+                                <td>
+                                    <div class="switch-wrapper">
+                                        <input :id="'hidden-' + item.name" type="checkbox" class="switch-checkbox"
+                                               v-model="item.hidden" required>
+                                        <label class="switch-label" :for="'hidden-' + item.name">hidden</label>
+                                    </div>
+                                </td>
+                                <td>
+                                    <i class="popover-button fas fa-trash-alt" aria-hidden="true"
+                                       @click="removeRow(index)"></i>
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
+                        <button type="button" class="btn btn-success" @click="addColumn()">Add new column</button>
                     </div>
                     <div class="card-footer">
-                        <button type="submit" class="btn btn-primary" @click="submitForm">Submit
-                        </button>
+                        <button type="submit" class="btn btn-primary" @click="submitForm">Submit</button>
                         <span id='valid_indicator' class="float-right"></span>
                     </div>
                 </div>
@@ -33,31 +79,35 @@
 
 	export default {
 		mixins: [TextFilter],
-        data() {
-		    return {
-		        title: "",
-                test: {
-		            'title': ""
-                }
-            }
-        },
+		data() {
+			return {
+				title: "",
+				data: {
+					'title': "",
+					items: []
+				}
+			}
+		},
 		methods: {
-			scrollEvent() {
-				$(".wrapper").get(0).scrollIntoView({behavior: 'smooth'});
-			},
 			submitForm() {
 
 			},
-		},
-		mounted() {
-			this.scrollEvent();
+			addColumn() {
+				this.data.items.push({
+					name: "",
+					type: "",
+					description: "",
+					default: "",
+					hidden: false
+				});
+			},
+			removeRow(index) {
+				this.data.items.splice(index, 1);
+			}
 		},
 		computed: {
-
-		},
-		watch: {
-			'$route.params.name'() {
-				this.loadData();
+			itemsExists() {
+				return !!this.data.items && this.data.items.length > 0;
 			}
 		}
 	}
