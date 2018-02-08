3c44e2a43376c5b0f6f3c0239b27b7ed1eb44eec
Tjappo <Y.J.Hu@student.tudelft.nl>
2018-02-08 17:51:58 +0100

    added input validation

--- resources/assets/js/components/schema/Create.vue
+++ resources/assets/js/components/schema/Create.vue
@@ -10,63 +10,62 @@
                     <div class="card-header">
                         Create new schema: <strong>{{data.title}}</strong>
                     </div>
-                    <div class="card-body">
-                        <div class="form-group">
-                            <label for="title">Title</label>
-                            <input id="title" type="text" class="form-control" v-model="data.title">
+                    <form>
+                        <div class="card-body">
+                            <div class="form-group">
+                                <label for="title">Title</label>
+                                <input id="title" type="text" class="form-control" v-model="data.title" required>
+                            </div>
+                            <table class="table" v-if="itemsExists">
+                                <thead>
+                                <tr>
+                                    <td><strong>Name</strong></td>
+                                    <td><strong>Type</strong></td>
+                                    <td><strong>Description</strong></td>
+                                    <td><strong>Default value</strong></td>
+                                    <td><strong>Hidden</strong></td>
+                                    <td></td>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <tr v-for="(item, index) in data.items">
+                                    <td>
+                                        <input type="text" class="form-control" placeholder="Name" v-model="item.name" required>
+                                    </td>
+                                    <td>
+                                        <select class="form-control" required v-model="item.type">
+                                            <option v-for="(value, key) in types" :value="key">{{value}}</option>
+                                        </select>
+                                    </td>
+                                    <td>
+                                        <input type="text" class="form-control" placeholder="Description"
+                                               v-model="item.description">
+                                    </td>
+                                    <td>
+                                        <input type="text" class="form-control" placeholder="Default value">
+                                    </td>
+                                    <td>
+                                        <div class="switch-wrapper">
+                                            <input :id="'hidden-' + item.name" type="checkbox" class="switch-checkbox"
+                                                   v-model="item.hidden">
+                                            <label class="switch-label" :for="'hidden-' + item.name">hidden</label>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <i class="popover-button fas fa-trash-alt" aria-hidden="true"
+                                           @click="removeRow(index)"></i>
+                                    </td>
+                                </tr>
+                                </tbody>
+                            </table>
+                            <button type="button" class="btn btn-success" @click="addColumn()">Add new column</button>
                         </div>
-                        <table class="table" v-if="itemsExists">
-                            <thead>
-                            <tr>
-                                <td><strong>Name</strong></td>
-                                <td><strong>Type</strong></td>
-                                <td><strong>Description</strong></td>
-                                <td><strong>Default value</strong></td>
-                                <td><strong>Hidden</strong></td>
-                                <td></td>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            <tr v-for="(item, index) in data.items">
-                                <td>
-                                    <input type="text" class="form-control" placeholder="Name" v-model="item.name">
-                                </td>
-                                <td>
-                                    <select class="form-control" required v-model="item.type">
-                                        <option value="string" selected>String</option>
-                                        <option value="integer">Number</option>
-                                        <option value="boolean">Boolean</option>
-                                        <option value="upload">Media upload</option>
-                                        <option value="html">HTML</option>
-                                    </select>
-                                </td>
-                                <td>
-                                    <input type="text" class="form-control" placeholder="Description"
-                                           v-model="item.description">
-                                </td>
-                                <td>
-                                    <input type="text" class="form-control" placeholder="Default value">
-                                </td>
-                                <td>
-                                    <div class="switch-wrapper">
-                                        <input :id="'hidden-' + item.name" type="checkbox" class="switch-checkbox"
-                                               v-model="item.hidden">
-                                        <label class="switch-label" :for="'hidden-' + item.name">hidden</label>
-                                    </div>
-                                </td>
-                                <td>
-                                    <i class="popover-button fas fa-trash-alt" aria-hidden="true"
-                                       @click="removeRow(index)"></i>
-                                </td>
-                            </tr>
-                            </tbody>
-                        </table>
-                        <button type="button" class="btn btn-success" @click="addColumn()">Add new column</button>
-                    </div>
-                    <div class="card-footer">
-                        <button type="submit" class="btn btn-primary" @click="submitForm">Submit</button>
-                        <span id='valid_indicator' class="float-right"></span>
-                    </div>
+                        <div class="card-footer">
+                            <input type="submit" class="btn btn-primary" @click="submitForm" value="Submit"
+                                   :disabled="validateForm()">
+                            <span id='valid_indicator' class="float-right"></span>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
@@ -74,46 +73,71 @@
 </template>
 
 <script>
-	import TextFilter from '../filters/textFilters.js';
+    import TextFilter from '../filters/textFilters.js';
 
-	export default {
-		mixins: [TextFilter],
-		data() {
-			return {
-				title: "",
-				data: {
-					'title': "",
-					items: []
-				}
-			}
-		},
-		methods: {
-			validateForm() {
-				// validate
+    export default {
+        mixins: [TextFilter],
+        data() {
+            return {
+                title: "",
+                data: {
+                    'title': "",
+                    items: []
+                },
+                types: {
+                    "string": "String",
+                    "integer": "Number",
+                    "boolean": "Boolean",
+                    "upload": "Media upload",
+                    "html": "HTML"
+                }
+            }
+        },
+        methods: {
+            validateForm() {
+                const sanitized = this.validateString(this.data.title);
+                if (!!sanitized) return; // throw error: title not sanitized
+
+                this.data.title = sanitized;
+
+                for (let item of this.data.items) {
+                    item.default = this.validateString(item.default);
+                    item.description = this.validateString(item.description);
+                    item.hidden = (!!item.hidden);
+                    item.name = this.validateString(item.name);
+                    item.type = this.validateString(item.type);
+                    if (item.type in this.types) return;
+                }
+
+                return true;
             },
-			submitForm() {
-				this.validateForm();
+            submitForm() {
+                this.validateForm();
                 // submit
-			},
-			addColumn() {
-				this.data.items.push({
-					name: "",
-					type: "",
-					description: "",
-					default: "",
-					hidden: false
-				});
-			},
-			removeRow(index) {
-				this.data.items.splice(index, 1);
-			}
-		},
-		computed: {
-			itemsExists() {
-				return !!this.data.items && this.data.items.length > 0;
-			}
-		}
-	}
+            },
+            addColumn() {
+                this.data.items.push({
+                    name: "",
+                    type: "",
+                    description: "",
+                    default: "",
+                    hidden: false
+                });
+            },
+            removeRow(index) {
+                this.data.items.splice(index, 1);
+            },
+            validateString(str) {
+                const sanitized = this.$options.filters.sanitizeString(str);
+                return (!!sanitized && typeof str === 'string') ? sanitized : '';
+            }
+        },
+        computed: {
+            itemsExists() {
+                return !!this.data.items && this.data.items.length > 0;
+            }
+        }
+    }
 </script>
 
 <style>
