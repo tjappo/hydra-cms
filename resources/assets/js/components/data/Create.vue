dfa066cca3dabe1a5b7b25781962c594c5ee4ed0
Jasper Hu <Y.J.Hu@student.tudelft.nl>
2018-02-25 16:49:55 +0100

    updated valid indicator style

--- resources/assets/js/components/data/Create.vue
+++ resources/assets/js/components/data/Create.vue
@@ -17,7 +17,7 @@
                         <button type="submit" class="btn btn-primary" @click="submitForm" :disabled="hasErrors">Submit
                         </button>
                         <b-link class="btn btn-secondary float-right" :to="{name: 'Index'}">Cancel</b-link>
-                        <span id='valid_indicator' class="float-right"></span>
+                        <span id='valid_indicator' class="mt-1 mr-3 float-right"></span>
                     </div>
                 </div>
             </div>
