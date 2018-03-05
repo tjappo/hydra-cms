1ed1c3ccc48cc5f57052aacdd0d726b4988d0a65
Jasper Hu <Y.J.Hu@student.tudelft.nl>
2018-03-05 22:28:38 +0100

    fixed export path directory

--- config.mjs
+++ config.mjs
@@ -3,7 +3,7 @@ import config from './config.global.mjs';
 /**
  * CONFIGURATION
  */
-config.exportPath = 'export/';
+config.exportPath = 'export/data/';
 config.ignoreFolders = ['img'];
 
 export default config;
