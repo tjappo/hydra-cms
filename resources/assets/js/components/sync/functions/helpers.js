c52ac0cc1b7858998ed90dfd5c1d7302fea12c04
Jasper Hu <Y.J.Hu@student.tudelft.nl>
2018-05-26 13:47:09 +0200

    fixed file diff

--- resources/assets/js/components/sync/functions/helpers.js
+++ resources/assets/js/components/sync/functions/helpers.js
@@ -1,13 +1,13 @@
 export default {
     methods: {
         constructFile(item) {
-            const data = window[item.Name || item + 'Data'],
-                schema = window[item.Name || item + 'Schema'];
+            const data = window[(item.Name || item) + 'Data'],
+                schema = window[(item.Name || item) + 'Schema'];
 
             if (!data || !schema) return;
 
-            const dataToPush = "window['" + item.Name || item + "Data'] = " + JSON.stringify(data, null, "\t") + ';',
-                schemaToPush = "window['" + item.Name || item + "Schema'] = " + JSON.stringify(schema, null, "\t") + ';',
+            const dataToPush = "window['" + (item.Name || item) + "Data'] = " + JSON.stringify(data, null, "\t") + ';',
+                schemaToPush = "window['" + (item.Name || item) + "Schema'] = " + JSON.stringify(schema, null, "\t") + ';',
                 offset = "\n\n// IMPORTANT:\n" +
                     "//  title has to be the same as the name of the data variable\n" +
                     "//  Url to this file relatively to the root folder\n";
